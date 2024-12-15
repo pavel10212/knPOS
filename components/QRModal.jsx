e711d48a3cea8a0ae53b40ef32ee3934a59c6855
Pavel <u6430176@au.edu>
2024-12-15 11:05:46 +0700

    New link format

--- components/QRModal.jsx
+++ components/QRModal.jsx
@@ -1,50 +1,53 @@
 import { useEffect, useState } from "react";
-import { Modal, Text, TouchableOpacity, View } from "react-native";
+import { Modal, Text, TouchableOpacity, View, ActivityIndicator } from "react-native";
 import QRCode from 'react-native-qrcode-svg';
 
 const QRModal = ({ visible, onClose, table_num }) => {
   const [qrValue, setQrValue] = useState('Please wait...');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const fetchToken = async () => {
-      if (!table_num) {
-        setQrValue('Invalid table number');
-        return;
-      }
-
-      try {
-        const url = `http://${process.env.EXPO_PUBLIC_IP}:3000/generate-token`;
-
-        const response = await fetch(url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ table_num }),
-        });
-
-
-        if (!response.ok) {
-          const errorText = await response.text();
-          console.error('Server response:', errorText);
-          throw new Error(`Server error: ${response.status}`);
+    if (visible) {
+      console.log('Fetching token for table:', table_num);
+      const fetchToken = async () => {
+        if (!table_num) {
+          setQrValue('Invalid table number');
+          return;
         }
 
-        const data = await response.json();
+        setIsLoading(true);
+        try {
+          const url = `http://${process.env.EXPO_PUBLIC_IP}:3000/generate-token`;
+          const response = await fetch(url, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ table_num }),
+          });
 
-        if (!data.url) {
-          throw new Error('No URI in response');
+          if (!response.ok) {
+            const errorText = await response.text();
+            console.error('Server response:', errorText);
+            throw new Error(`Server error: ${response.status}`);
+          }
+
+          const data = await response.json();
+          if (!data.url) {
+            throw new Error('No URI in response');
+          }
+          setQrValue(data.url);
+        } catch (error) {
+          console.error('Error fetching token:', error);
+          setQrValue('Error generating QR code');
+        } finally {
+          setIsLoading(false);
         }
+      };
 
-        setQrValue(data.url);
-      } catch (error) {
-        console.error('Error fetching token:', error);
-        setQrValue('Error generating QR code');
-      }
-    };
-
-    fetchToken();
-  }, [table_num]);
+      fetchToken();
+    }
+  }, [visible, table_num]);
 
   return (
     <Modal
@@ -57,10 +60,14 @@ const QRModal = ({ visible, onClose, table_num }) => {
         <View className="bg-white p-6 rounded-xl w-[80%] items-center">
           <Text className="text-lg font-semibold mb-4">Scan this QR code to view the menu</Text>
           <View className="bg-white p-4 rounded-lg">
-            <QRCode
-              value={qrValue}
-              size={200}
-            />
+            {isLoading ? (
+              <ActivityIndicator size="large" color="#0000ff" />
+            ) : (
+              <QRCode
+                value={qrValue}
+                size={200}
+              />
+            )}
           </View>
           <TouchableOpacity
             className="mt-4 bg-primary px-6 py-2 rounded-lg"
